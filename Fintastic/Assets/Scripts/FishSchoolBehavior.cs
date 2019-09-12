8861ee2195424ecf08f69c27fc731f065209f87a
Orgnodor <max3@kth.se>
2019-09-12 16:47:50 +0200

    Vivepro session in VIC - Added: fog, some more interactable objects and a teleportation area, fish models, the beginings of a fish school behavior.

---
+++ Fintastic/Assets/Scripts/FishSchoolBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSchoolBehavior : MonoBehaviour
+{
+    private List<GameObject> fishes;
+    public Transform target;
+    public float fishSpeed = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fishes = new List<GameObject>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject go = transform.GetChild(i).gameObject;
+            if (go && go.tag == "Fish")
+            {
+                fishes.Add(go);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var fish in fishes)
+        {
+            fish.transform.LookAt(target);
+            fish.transform.position = fish.transform.position + fish.transform.forward * fishSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ Fintastic/Assets/Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float rotationSpeed = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 1, 0) * rotationSpeed * Time.deltaTime);
+    }
+}
