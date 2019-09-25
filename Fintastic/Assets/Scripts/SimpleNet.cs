d3d6abb44a3beff2d3f698d296643b9b78d1d7b7
Orgnodor <max3@kth.se>
2019-09-25 13:45:44 +0200

    minor net changes

--- Fintastic/Assets/Scripts/SimpleNet.cs
+++ Fintastic/Assets/Scripts/SimpleNet.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class SimpleNet : MonoBehaviour
 {
+    void Start() {
+        Rigidbody rb = transform.parent.gameObject.GetComponent<Rigidbody>();
+        rb.centerOfMass = new Vector3(0, -0.367f, 0);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collission!!!");
